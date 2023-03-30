41be3b3539cf67f5805949d2cb58fb3f4450696f
King Maningo <king@inboundfound.com>
2023-03-31 04:41:37 +0800

    First commit
    Enqueued style

---
+++ if-blocks-styles-if.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Plugin Name: IF Blocks Styles Addon (InboundFound)
+ * Description: Custom styles made specifically for InboundFound site.
+ * Author: Inbound Found / David Rodrigues
+ * Version: 1.0.0
+ * GitHub Plugin URI: https://github.com/king-inboundfound/if-blocks-styles-if
+ * Primary Branch: main
+ * Text Domain: if-blocks-styles-if
+ */
+if (!defined('ABSPATH')) { return; }
+
+function ifBlocksEnqueueScriptsIf() {
+  wp_enqueue_style('if-blocks-styles-if', plugin_dir_url(__FILE__) . 'assets/css/style.css',
+    array(), filemtime(__DIR__ . '/assets/css/style.css'));
+};
+add_action('wp_enqueue_scripts', 'ifBlocksEnqueueScriptsIf', 10);
